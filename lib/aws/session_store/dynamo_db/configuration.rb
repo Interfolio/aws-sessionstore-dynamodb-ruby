34b544bcb8da4c5e2a7e0d23d40b876ec0d4de86
Greg Wilson <greg.wilson@interfolio.com>
2017-01-13 10:02:10 -0500

    Store in JSON instead of Marshal - Added config option

--- lib/aws/session_store/dynamo_db/configuration.rb
+++ lib/aws/session_store/dynamo_db/configuration.rb
@@ -56,6 +56,7 @@ module AWS::SessionStore::DynamoDB
     DEFAULTS = {
       :table_name => "sessions",
       :table_key => "session_id",
+      :storage_format => "marshal",
       :consistent_read => true,
       :read_capacity => 10,
       :write_capacity => 5,
@@ -75,6 +76,10 @@ module AWS::SessionStore::DynamoDB
 
     # @return [String] Session table hash key name.
     attr_reader :table_key
+
+    # @return [String] Format data is stored in before encoded and written to
+    #   the database.
+    attr_reader :storage_format
 
     # @return [true] If a strongly consistent read is used
     # @return [false] If an eventually consistent read is used.
@@ -142,6 +147,8 @@ module AWS::SessionStore::DynamoDB
     #   table.
     # @option options [String] :table_key ("id") The hash key of the sesison
     #   table.
+    # @option options [String] :storage_format ("marshal") Format data is stored
+    #   in before encoded and written to the database.
     # @option options [Boolean] :consistent_read (true) If true, a strongly
     #   consistent read is used. If false, an eventually consistent read is
     #   used.
